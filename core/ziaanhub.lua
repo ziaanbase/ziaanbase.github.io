c5b8c5d92a2dce180449df6849121f8a2809a844
ziaanbase <picaaaaaaaaaaaaa@gmail.com>
2025-11-11 23:07:42 +0700

    Update ziaanhub.lua

--- core/ziaanhub.lua
+++ core/ziaanhub.lua
@@ -1,20 +1,20 @@
 local Games = {
-  [82321750197896] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/Games/PBB/csx.lua", -- PoopABrainRot
-  [113604074601559] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/Games/BAH/css.lua",  -- "Build a Beehive
-  [2753915549] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/ziaan.lua", -- Blox Fruit Sea 1
-  [4442272183] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/ziaan.lua", -- Blox Fruit Sea 2
-  [7449423635] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/Games/BF/ziaan.lua", -- Blox Fruit Sea 3  
-  [10324346056] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- evade Big Team
-  [9872472334] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- evade EVADE
-  [10662542523] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- evade Casual
-  [10324347967] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- evade Social Space
-  [121271605799901] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- evade Player Nextbots
-  [10808838353] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- evade VC Only
-  [11353528705] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua",  -- evade Pro
-  [99214917572799] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/Games/error.lua", -- evade Custom Servers
-  [12334109280] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/C/Games/GBD/vsp.lua", -- Guns & Black Powder
-  [14216737767] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/B/C/Games/GBD/vsp.lua", -- Guns & Black Powder Mobile
-  [93978595733734] = "https://raw.githubusercontent.com/ziaanhub/ziaanhub/refs/heads/main/source/default/A/B/C/m.lua", -- Violence District 
+  [82321750197896] = "https://ziaanbase.github.io/games/Poop%20a%20Brainrot/main.lua", -- PoopABrainRot
+  [113604074601559] = "https://ziaanbase.github.io/games/Build%20a%20Beehive/main.lua",  -- "Build a Beehive
+  [2753915549] = "https://ziaanbase.github.io/games/Blox%20Fruit/main.lua", -- Blox Fruit Sea 1
+  [4442272183] = "https://ziaanbase.github.io/games/Blox%20Fruit/main.lua", -- Blox Fruit Sea 2
+  [7449423635] = "https://ziaanbase.github.io/games/Blox%20Fruit/main.lua", -- Blox Fruit Sea 3  
+  [10324346056] = "https://ziaanbase.github.io/games/Evade/main.lua", -- evade Big Team
+  [9872472334] = "https://ziaanbase.github.io/games/Evade/main.lua", -- evade EVADE
+  [10662542523] = "https://ziaanbase.github.io/games/Evade/main.lua", -- evade Casual
+  [10324347967] = "https://ziaanbase.github.io/games/Evade/main.lua", -- evade Social Space
+  [121271605799901] = "https://ziaanbase.github.io/games/Evade/main.lua", -- evade Player Nextbots
+  [10808838353] = "https://ziaanbase.github.io/games/Evade/main.lua", -- evade VC Only
+  [11353528705] = "https://ziaanbase.github.io/games/Evade/main.lua",  -- evade Pro
+  [99214917572799] = "https://ziaanbase.github.io/games/Evade/main.lua", -- evade Custom Servers
+  [12334109280] = "https://ziaanbase.github.io/games/Guts%20and%20Blackpowder/main.lua", -- Guns & Black Powder
+  [14216737767] = "https://ziaanbase.github.io/games/Guts%20and%20Blackpowder/main.lua", -- Guns & Black Powder Mobile
+  [93978595733734] = "https://ziaanbase.github.io/games/Violence%20District/main.lua", -- Violence District 
 }
 
 return Games
