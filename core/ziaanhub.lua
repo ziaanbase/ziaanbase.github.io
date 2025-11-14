56a77f1434dd2c575507713b63d0392b7e436287
ziaanbase <picaaaaaaaaaaaaa@gmail.com>
2025-11-14 09:56:32 +0700

    Update ziaanhub.lua

--- core/ziaanhub.lua
+++ core/ziaanhub.lua
@@ -1,16 +1,13 @@
 local Games = {
-  [82321750197896] = "https://ziaanbase.github.io/games/Poop%20a%20Brainrot/main.lua", -- PoopABrainRot
-  
+  [82321750197896] = "https://ziaanbase.github.io/games/Poop%20a%20Brainrot/main.lua", -- PoopABrainRot  
   [113604074601559] = "https://ziaanbase.github.io/games/Build%20a%20Beehive/main.lua",  -- "Build a Beehive
-  [113604074601559] = "https://ziaanbase.github.io/games/Build%20a%20Beehive/Notify/main.lua",  -- "BAB Notify
-  
+  [113604074601559] = "https://ziaanbase.github.io/games/Build%20a%20Beehive/Notify/main.lua",  -- "BAB Notify  
   [2753915549] = "https://ziaanbase.github.io/games/Blox%20Fruit/main.lua", -- Blox Fruit Sea 1
   [4442272183] = "https://ziaanbase.github.io/games/Blox%20Fruit/main.lua", -- Blox Fruit Sea 2
   [7449423635] = "https://ziaanbase.github.io/games/Blox%20Fruit/main.lua", -- Blox Fruit Sea 3  
   [2753915549] = "https://ziaanbase.github.io/games/Blox%20Fruit/Notify/main.lua", -- Blox Fruit Sea 1
   [4442272183] = "https://ziaanbase.github.io/games/Blox%20Fruit/Notify/main.lua", -- Blox Fruit Sea 2
-  [7449423635] = "https://ziaanbase.github.io/games/Blox%20Fruit/Notify/main.lua", -- Blox Fruit Sea 3  
-  
+  [7449423635] = "https://ziaanbase.github.io/games/Blox%20Fruit/Notify/main.lua", -- Blox Fruit Sea 3    
   [10324346056] = "https://ziaanbase.github.io/games/Evade/main.lua", -- evade Big Team
   [9872472334] = "https://ziaanbase.github.io/games/Evade/main.lua", -- evade EVADE
   [10662542523] = "https://ziaanbase.github.io/games/Evade/main.lua", -- evade Casual
@@ -19,6 +16,14 @@ local Games = {
   [10808838353] = "https://ziaanbase.github.io/games/Evade/main.lua", -- evade VC Only
   [11353528705] = "https://ziaanbase.github.io/games/Evade/main.lua",  -- evade Pro
   [99214917572799] = "https://ziaanbase.github.io/games/Evade/main.lua", -- evade Custom Servers
+  [10324346056] = "https://ziaanbase.github.io/games/Evade/Notify/main.lua", -- evade Big Team
+  [9872472334] = "https://ziaanbase.github.io/games/Evade/Notify/main.lua", -- evade EVADE
+  [10662542523] = "https://ziaanbase.github.io/games/Evade/Notify/main.lua", -- evade Casual
+  [10324347967] = "https://ziaanbase.github.io/games/Evade/Notify/main.lua", -- evade Social Space
+  [121271605799901] = "https://ziaanbase.github.io/games/Evade/Notify/main.lua", -- evade Player Nextbots
+  [10808838353] = "https://ziaanbase.github.io/games/Evade/Notify/main.lua", -- evade VC Only
+  [11353528705] = "https://ziaanbase.github.io/games/Evade/Notify/main.lua",  -- evade Pro
+  [99214917572799] = "https://ziaanbase.github.io/games/Evade/Notify/main.lua", -- evade Custom Servers  
   [12334109280] = "https://ziaanbase.github.io/games/Guts%20and%20Blackpowder/main.lua", -- Guns & Black Powder
   [14216737767] = "https://ziaanbase.github.io/games/Guts%20and%20Blackpowder/main.lua", -- Guns & Black Powder Mobile
   
